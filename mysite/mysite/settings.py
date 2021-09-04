1f0c58525c28aaef75af17408df37f72ec0c7dcd
Victor28 <sea810749@gmail.com>
2021-09-04 10:55:22 +0800

    my rollcall app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -154,7 +154,7 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-SITE_ID = 2
+SITE_ID = 3
 
 LOGIN_REDIRECT_URL = '/'
 #LOGOUT_REDIRECT_URL = '/'
